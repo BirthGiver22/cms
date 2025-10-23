98276462abc52b778ee546b11946ed2993c0e5a6
guptaRishi00 <rishigupta54066@gmail.com>
2025-10-24 01:00:55 +0530

    feat: add changes

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -476,6 +476,7 @@ export interface ApiGlobalGlobal extends Struct.SingleTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     cta: Schema.Attribute.Component<'shared.cta', true>;
+    footer: Schema.Attribute.Component<'shared.footer', false>;
     header: Schema.Attribute.Component<'shared.header', false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
